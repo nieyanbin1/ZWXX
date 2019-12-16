ebc58e7637ffa70e92a8222cfaeed8d5a5686bce
niyanbin <2857030058@qq.com>
2019-12-16 09:48:33 +0800

    聂艳彬数据库连接

---
+++ src/model/NybDButil.java
@@ -0,0 +1,79 @@
+package model;
+import java.sql.*;
+public class NybDButil {
+    public static Connection getConn() {
+        Connection con = null;
+        String url = "jdbc:mysql://localhost:3306/zwxx?serverTimezone=GMT%2B8";
+        String un = "root";
+        String pd = "123456";
+        try {
+            String str = "NYC";
+            //反射加载jdbc的Driver类
+            Class.forName("com.mysql.jdbc.Driver");
+            //通过DriverManager获取Connection对象
+            con = DriverManager.getConnection(url, un, pd);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public static ResultSet getResultSet(Statement stmt, String sql) {
+        ResultSet rs = null;
+        try {
+            if(stmt != null) {
+                rs = stmt.executeQuery(sql);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public  static PreparedStatement getPreparedStatement(String sql , Connection conn){
+        PreparedStatement statement = null;
+        try {
+            if(conn != null) {
+                statement = conn.prepareStatement(sql);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return  statement;
+    }
+
+    public static Statement getStatement(Connection conn) {
+        Statement  statement = null;
+        try {
+            if(conn != null) {
+                statement = conn.createStatement();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return  statement;
+    }
+
+    public static void close() {
+        ResultSet rs = null;
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            rs.close();
+//					System.out.println("rs closed");
+            if (ps != null) {
+                ps.close();
+                ps = null;
+//						System.out.println("ps closed");
+                if (conn != null) {
+                    conn.close();
+                    conn = null;
+//							System.out.println("conn closed");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
