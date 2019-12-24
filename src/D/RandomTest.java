2fe0be40df4a3162e35bc14d5c082af0a6cf32b3
zhoumuwang <56620081+zhoumuwang@users.noreply.github.com>
2019-12-24 09:30:33 +0800

    随机排队号生成算法

--- src/D/RandomTest.java
+++ src/D/RandomTest.java
@@ -1,4 +1,26 @@
 package D;
 
-public class Random {
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.*;
+public class RandomTest{
+    public static Set<Integer>GetRandomNumber() {
+        Set<Integer> set= new HashSet<Integer>();
+        Random random =new Random();
+        while(set.size()<4){
+            set.add(random.nextInt(10));
+
+        }
+        return set;
+    }
+    public static void main(String[] args) {
+        Set<Integer> set =GetRandomNumber();
+        Iterator<Integer> iterator =set.iterator();
+        String temp="";
+        while(iterator.hasNext()) {
+            temp+=iterator.next();
+        }
+        System.out.print(temp);
+    }
 }
